4d7043d9b6cc68b6813d6a2ee9cf3f633d09cc85
santiago galván <sgalsant@gmail.com>
2025-07-20 18:23:18 +0100

    ok

--- postscript/wsl.ps1
+++ postscript/wsl.ps1
@@ -4,10 +4,11 @@
 
  Enable-windowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
 
-wsl --set-default-version 2
+ # los siguientes pasos se deben hacer tras reiniciar windows
+# wsl --set-default-version 2
 
-$parentDir = Split-Path -Path $PSScriptRoot -Parent
-$repoPath = Join-Path -Path $parentDir -ChildPath "repo"
+# $parentDir = Split-Path -Path $PSScriptRoot -Parent
+# $repoPath = Join-Path -Path $parentDir -ChildPath "repo"
 
 
-wsl --import ubuntu c:/ubuntu $repoPath/ubuntu-24.04.2-wsl-amd64.gz --version 2
+# wsl --import ubuntu c:/ubuntu $repoPath/ubuntu-24.04.2-wsl-amd64.gz --version 2
