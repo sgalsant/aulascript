dcec09f8a5f8250de52991b2a4a8f3c33b983a5b
santiago galván <sgalsant@gmail.com>
2025-07-20 13:50:29 +0100

    refactorización

--- 1-crear-cuentas-instalar-aplicaciones.bat
+++ 1-crear-cuentas-instalar-aplicaciones.bat
@@ -2,6 +2,6 @@
 
 PowerShell -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0cuentas-usuario.ps1\"' -Verb RunAs"
 
-PowerShell -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0instalar.ps1\"' -Verb RunAs"
+PowerShell -ExecutionPolicy Bypass -Command "Start-Process PowerShell -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0instalar-aplicaciones.ps1\"' -Verb RunAs"
 
 pause
